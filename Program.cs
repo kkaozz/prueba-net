aef00454c7bb275d38f88849de9a77522b0907d5
lalo flores <prueba@correo.com>
2023-12-18 11:44:45 -0700

    agregue los archivos de mi carpeta NET de prueba

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current time is limit, enjoy your life" + DateTime.Now);
